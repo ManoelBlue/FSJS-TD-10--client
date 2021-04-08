e3e1468b903c7e8b6760064718ab878b5b04e4d3
Manoel Cunha <cunha@cngroup.dk>
2021-04-08 16:46:02 +0200

    fetch api in client side

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React, {useEffect} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    fetch('http://localhost:5000/api/courses')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  });
+
   return (
     <div className="App">
     </div>
